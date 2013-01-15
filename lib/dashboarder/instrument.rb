12fe904aa2fdbc00ac64601c526f03347d85a021
arunthampi <arun.thampi@gmail.com>
2013-01-15 03:24:49 +0000

    Ability to set source for instrument Use Ruby 1.9 hash syntax

--- lib/dashboarder/instrument.rb
+++ lib/dashboarder/instrument.rb
@@ -1,16 +1,16 @@
 module Dashboarder
   module Instrument
     def self.get(name)
-      Dashboarder.api.get('/v1/instruments', :query => {:name => name})['instruments'].first
+      Dashboarder.api.get('/v1/instruments', :query => {name: name})['instruments'].first
     end
 
-    def self.create!(name, metric_names)
-      streams = metric_names.map { |n| { :metric => n, :source => '*' } }
-      Dashboarder.api.post('/v1/instruments', { :name => name, :streams => streams })
+    def self.create!(name, metric_names, source)
+      streams = metric_names.map { |n| { metric: n, source: source } }
+      Dashboarder.api.post('/v1/instruments', { name: name, streams: streams })
     end
 
-    def self.compose(name, metric_names)
-      get(name) || create!(name, metric_names)
+    def self.compose(name, metric_names, source = '*')
+      get(name) || create!(name, metric_names, source)
     end
   end
 end

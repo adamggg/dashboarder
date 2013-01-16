c3a6a41776a714ec0ed7293bec73300f21f31671
Daniel, Dao Quang Minh <dqminh89@gmail.com>
2013-01-16 09:15:29 +0000

    add create_or_update! for instrument

--- lib/dashboarder/instrument.rb
+++ lib/dashboarder/instrument.rb
@@ -12,5 +12,18 @@ module Dashboarder
     def self.compose(name, metric_names, source = '*')
       get(name) || create!(name, metric_names, source)
     end
+
+    def self.update(id, name, metric_names, source)
+      streams = metric_names.map { |n| { metric: n, source: source } }
+      Dashboarder.api.put("/v1/instruments/#{id}", { name: name, streams: streams })
+    end
+
+    def self.create_or_update!(name, metric_names, source)
+      if existing_instrument = get(name)
+        update existing_instrument["id"], name, metric_names, source
+      else
+        create! name, metric_names, source
+      end
+    end
   end
 end
